8cb2912be0dc7636ee96224ccc109ad221022d8c
Barbara <barbeaboo@gmail.com>
2024-10-18 05:08:02 -0700

    Task 4,5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+A module that adds lists
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ A function that returns the sum of a list of floats """
+    return sum(input_list)
