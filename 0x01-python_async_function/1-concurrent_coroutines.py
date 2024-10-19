f66b6c7e81400318d12ebde535e90adb5316428f
Barbara <barbeaboo@gmail.com>
2024-10-18 21:53:26 -0700

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-A module 
+A module that returns a list of delays
 """
 import asyncio
 from typing import List
@@ -9,12 +9,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """A coroutine that generates a float between 0 and max_delay"""
+    """A coroutine that generates a liist of float between n and max_delay"""
     wait = []
     td = [wait_random(max_delay) for _ in range(n)]
-    
     for i in asyncio.as_completed(td):
         delay = await i
         wait.append(delay)
-    
     return wait
